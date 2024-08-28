51e79f5780bc4364abf4bd6f5948bd04b92e8629
Seungyeon Oh <seungyeon_oh@hotmail.com>
2024-08-28 22:08:54 +0900

    [Gold IV] Title: 사냥꾼, Time: 48 ms, Memory: 4204 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.cc
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.cc
@@ -10,22 +10,55 @@ vector<pair<int, int>> animals;
 void solution()
 {
 	int count = 0;
-	for (int i = 0; i < N; i++)
+
+	int first = 0;
+	int last = N - 1;
+	int bound = first;
+	
+	for (int i = 1; i < M; i++)
 	{
-		for (int j = 0; j < M; j++)
+		int target = (position[i] + position[i - 1]) / 2;
+		int prevBound = bound;
+		while (first <= last)
 		{
-			int distance = abs(position[j] - animals[i].first) + animals[i].second;
-			if (distance <= L)
+			int mid = (first + last) / 2;
+
+			if (animals[mid].first > target)
+				last = mid - 1;
+			else
 			{
-				count++;
-				break;
+				first = mid + 1;
+				bound = max(mid + 1, bound);
 			}
 		}
+
+		for (int j = prevBound; j < bound; j++)
+		{
+			if (abs(position[i - 1] - animals[j].first) + animals[j].second <= L)
+				count++;
+		}
+		first = bound;
+		last = N - 1;
 	}
+
+	for (int i = bound; i < N; i++)
+	{
+		if (abs(position[M-1] - animals[i].first) + animals[i].second <= L)
+			count++;
+	}
+
 	cout << count;
 }
 
+bool CompareAnimal(pair<int, int> A, pair<int, int> B)
+{
+	return (A.first < B.first);
+}
 
+bool ComparePosition(int A, int B)
+{
+	return (A < B);
+}
 
 int main()
 {
@@ -39,7 +72,7 @@ int main()
 		position.push_back(data);
 	}
 
-
+	sort(position.begin(), position.end(), ComparePosition);
 
 	for (int i = 0; i < N; i++)
 	{	
@@ -47,6 +80,8 @@ int main()
 		cin >> X >> Y;
 		animals.push_back(make_pair(X, Y));
 	}
+
+	sort(animals.begin(), animals.end(), CompareAnimal);
 
 	solution();
 }
